2c84d78d5298d5c6b54cd794e3021e045adff604
Dick Cheney <40896988+DickCheney23@users.noreply.github.com>
2018-07-09 08:11:52 -0700

    Update GatherPrinters.ps1

--- GatherPrinters.ps1
+++ GatherPrinters.ps1
@@ -21,57 +21,11 @@ foreach ($objResult in $colResults)
  $objPrinters += $objptr
  }
 
+#Show the array
 $objPrinters
+
 #Export the data to a CSV file
 $objPrinters | export-csv Printers.csv -NTI
+
+#View the data in a table
 $objPrinters | ft name, driver, server, description
-
-
-[pscustomobject]@{
-    ExtraInfo = (@(1,3,5,6) | Out-String).Trim()
-} | Export-Csv -notype Random.csv
-
-$Top5 = Get-Process -Computer $Item | Sort Handles -descending |Select -expand Handles | |Select -First 5 
- $new = [pscustomobject]@{ Top5 = (@($Top5) -join ',')
- }
-
-[pscustomobject]@{
-    First = 'Boe'
-    Last = 'Prox'
-    ExtraInfo = (@(1,3,5,6) | Out-String).Trim()
-    State = 'NE'
-    IPs = (@('111.222.11.22','55.12.89.125','125.48.2.1','145.23.15.89','123.12.1.0') | Out-String).Trim()
-} | Export-Csv -notype Random.csv
- 
-$Printers = [pscustomobject]@{
-	objPrinters = (@($objPrinters) -join ',')
-	}
-
-$OutArray |Select-Object Name,@{Expression={$_.objPrinters.Name -join ';'}} #|Export-Csv test.csv
-$OutArray
-
-$Printers
-
-$NumColsToExport = 4
-
-
-#Create array to hold the data to be sent to the CSV file
-$Printers=@()
-$pNames=@("Name", "Driver", "Server", "Description")
-ForEach ($Row in $objPrinters){
-    $obj = New-Object PSObject
-    For ($i=0;$i -lt $NumColsToExport; $i++){
-              $obj | Add-Member -MemberType NoteProperty -Name $pNames[$i] -Value $Row[$i]
-      Echo $i
-      Echo $Row[$i]
-      }
-   $Printers+=$obj
-   $obj=$Null
-}
-
-#$Printers
-
-#Export the data to a CSV file
-$Printers | export-csv Printers.csv -NTI
-
-$Printers | ft name, driver, server, description
